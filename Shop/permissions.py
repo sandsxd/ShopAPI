9a67a7cf1e055f183da44cf74845e90cca059a19
sandsxd <44241090+sandsxd@users.noreply.github.com>
2019-05-22 22:53:43 -0400

    Delete permissions.py

--- Shop/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsCustomer(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.customer == request.user
-
-class IsUser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
